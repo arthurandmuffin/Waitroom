6fdf1ac1f610f862cae7c370fa9e4f4a4a647c17
Daniel Barroso <username@Daniels-Concession.local>
2025-01-25 15:14:59 -0500

    feat: updated gitignore and updated ALLOWED_HOSTS

--- Waitroom/settings.py
+++ Waitroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5)@qypafl^+qc=1azpjb5d&+%33bc@v0m7d!z5asg075jim6q6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
