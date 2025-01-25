fa132eda1bd1d5e441ce5f51aa15e75b5f495976
Daniel Barroso <username@Daniels-Concession.local>
2025-01-25 15:36:34 -0500

    settings.py updated with templates

--- Waitroom/settings.py
+++ Waitroom/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Waitroom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Waitroom/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
