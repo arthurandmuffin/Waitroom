1476ff2552bac42dece83a636333e8725ff47130
Arthur <56365708+arthurandmuffin@users.noreply.github.com>
2025-01-25 20:18:10 -0500

    feat: additional api endpoints for fe

--- api/models.py
+++ api/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+class Patient(models.Model):
+    patientID = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=255,
+        choices=[
+            ('Triaged', 'Triaged'),
+            ('Discharged', 'Discharged'),
+            ('Admitted', 'Admitted'),
+            ('Processing', 'Processing'),
+            ('Awaiting Tests', 'Awaiting Tests'),
+            ('MIA', 'MIA')
+        ],
+        default='Triaged'  # Default can be any value from the choices
+    )
+
 class Triage(models.Model):
     patientID = models.CharField(max_length=255)
     condition = models.TextField()
@@ -10,3 +25,32 @@ class Triage(models.Model):
     
     def __str__(self):
         return f"Patient ID: {self.patientID}\nCondition: {self.condition}\nTriage Category: {self.triageCategory}"
+    
+class Admitted(models.Model):
+    patientID = models.CharField(max_length=255)
+    physicianID = models.CharField(max_length=255)
+    admissionReason = models.TextField()
+    ward = models.IntegerField(validators=[MinValueValidator(1)])
+    admissionTime = models.DateTimeField()
+    
+class MissingPatient(models.Model):
+    patientID = models.CharField(max_length=255)
+    condition = models.TextField()
+    triageCategory = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    arrivalTime = models.DateTimeField()
+    estimatedTreatmentTime = models.IntegerField(validators=[MinValueValidator(1)])
+    
+class ProcessPatient(models.Model):
+    patientID = models.CharField(max_length=255)
+    roomNumber = models.IntegerField(validators=[MinValueValidator(1)])
+    startTime = models.DateTimeField()
+    estimatedEndTime = models.DateTimeField()
+    
+class Pending(models.Model):
+    lab = models.CharField(max_length=255, blank=True, null=True)
+    imaging = models.CharField(max_length=255, blank=True, null=True)
+    referral = models.CharField(max_length=255, blank=True, null=True)
+    
+class PendingPatient(models.Model):
+    patientID = models.CharField(max_length=255)
+    status = models.ForeignKey(Pending, on_delete=models.CASCADE)
